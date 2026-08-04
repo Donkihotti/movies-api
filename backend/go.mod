eb8814bb9c61b3088a22d7c7e3d3e0e86eea5f73
Tim Daniel Fiander <k-tfiande@c2r6p13.hive.fi>
2026-08-04 15:59:40 +0300

    Server started at localhost:8080

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module gitea.kood.tech/timdanielfiander/movies-api.git
+
+go 1.25.0
--- backend/main.go
+++ backend/main.go
@@ -1,10 +1,21 @@
 package main 
 
 import (
-	"fmt"
+	"net/http"
+	"log"
+
+	"gitea.kood.tech/timdanielfiander/movies-api.git/api"
 )
 
 func main() {
+	
+	router := api.NewRouter()
+
+	log.Println("Server running at port :8080")
+	err := http.ListenAndServe(":8080", router)
+		if err != nil {
+			log.Fatal("Starting server failed", err)	
+		}
 
 }
 
