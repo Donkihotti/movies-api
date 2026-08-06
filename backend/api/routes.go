41022ae8149b2be4a532aa1be9b623ddc357199f
Tim Daniel Fiander <k-tfiande@c2r6p13.hive.fi>
2026-08-06 15:57:36 +0300

    get movies endpoint works

--- backend/api/routes.go
+++ backend/api/routes.go
@@ -9,6 +9,7 @@ func NewRouter(h *Handler) *http.ServeMux {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", h.HomeHandler)
+	mux.HandleFunc("/movies", h.MoviesHandler)
 
 	return mux
 }
--- backend/database/migrations.go
+++ backend/database/migrations.go
@@ -2,23 +2,22 @@ package database
 
 import ( 
 	"database/sql" 
-	"fmt"
 	"os"
 	"path/filepath"
 	"slices"
+	"fmt"
 )
 
 func RunMigrations(db *sql.DB) error {
 
-	files, err := filepath.Glob("/database/migrations/* .sql")
+	files, err := filepath.Glob("database/migrations/*.sql")
 	if err != nil {
 		return err
 	}
 
 	slices.Sort(files)	
-	fmt.Println(files)
-
 	for _, file := range files {
+		fmt.Println("running mig")
 		content, err := os.ReadFile(file)
 		if err != nil {
 			return err
