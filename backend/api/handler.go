1f3442473b6608c52692b35a977997b9a17e7d6b
Tim Daniel Fiander <k-tfiande@c1r3p4.hive.fi>
2026-09-04 09:59:55 +0300

    Status OK for patch

--- backend/api/handler.go
+++ backend/api/handler.go
@@ -39,13 +39,14 @@ func WriteErrorStatus(w http.ResponseWriter, err error) {
 
 func WriteStatus(w http.ResponseWriter, method string) {
 	switch method {
-	case http.MethodGet:
+	case http.MethodGet, http.MethodPatch:
 		w.WriteHeader(http.StatusOK)
 	case http.MethodPost:
 		w.WriteHeader(http.StatusCreated)
-	case http.MethodDelete, http.MethodPatch:
+	case http.MethodDelete: 
 		w.WriteHeader(http.StatusNoContent)
 	default:
+
 		w.WriteHeader(http.StatusOK)
 	}
 	return
